c2de021ed950e64ca3db6e758fd32f480d2fdb0d
jklinck <joeklinck@hotmail.com>
2016-06-13 01:12:50 -0700

    added bootstrap to mywall.php

--- mywall.php
+++ mywall.php
@@ -5,18 +5,22 @@ session_start();
 <html lan="en">
 	<head>
 		<title>The Wall</title>
+		<!-- Latest compiled and minified CSS -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<!-- Optional theme -->
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
+		<!-- Latest compiled and minified JavaScript -->
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+		integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" 
+		crossorigin="anonymous"></script>
 		<meta charset="utf-8">
 		<style>
 			.red{
 				color:red;
 			}
-			body{
-				padding-left: 20px;
-				padding-top: 20px;
-			}
 		</style>
 	</head>
-	<!-- Registration errors -->
+		<!-- Registration errors -->
 		<div class="red">
 			<?php
 			if(isset($_SESSION['errors']))
@@ -39,53 +43,81 @@ session_start();
 			?>
 		</div>
 	<body>
-		<!-- registration -->
-		<h1>Registration</h1>
-		<form action="mywall_process.php" method="post">
-			<label for="first_name">First name: </label>
-			<input name="first_name" type="text"><br>
-
-			<label for="last_name">Last name: </label>
-			<input name="last_name" type="text"><br>
-
-			<label for="email">Email: </label>
-			<input name="email" type="email"><br>
-
-			<label for="password">Password: </label>
-			<input name="password" type="password"><br>
-
-			<label for="passconf">Password confirmation: </label>
-			<input name="passconf" type="password"><br>
-
-			<input type="hidden" name="action" value="register">
-			<input type="submit" value="Register">
-		</form>
-
-
-		<!-- Login errors -->
-		<div class="red">
-			<?php
-			if(isset($_SESSION['errors']))
-			{
-				foreach ($_SESSION['errors'] as $error2)
-				{
-					echo $error2 . "<br />";
-				}
-				unset($_SESSION['errors']);
-			}
-			?>
+		<div class="container-fluid">
+			<div class="jumbotron">
+				<div class="row">
+					<div class="col-xs-2">
+					</div>
+					<div class="col-xs-8">
+						<!-- registration -->
+						<h1>Registration</h1>
+						<form role="form" action="mywall_process.php" method="post">
+							<div class="form-group">
+								<label for="first_name">First name: </label>
+								<input name="first_name" type="text" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="last_name">Last name: </label>
+								<input name="last_name" type="text" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="email">Email: </label>
+								<input name="email" type="email" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="password">Password: </label>
+								<input name="password" type="password" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="passconf">Password confirmation: </label>
+								<input name="passconf" type="password" class="form-control">
+							</div>
+							<input type="hidden" name="action" value="register">
+							<button type="submit" class="btn btn-primary">Register</button>
+						</form>
+					</div>
+					<div class="col-xs-2">
+					</div>
+				</div>
+				<div class="row">
+					<!-- Login errors -->
+					<div class="red">
+						<?php
+						if(isset($_SESSION['errors']))
+						{
+							foreach ($_SESSION['errors'] as $error2)
+							{
+								echo $error2 . "<br />";
+							}
+							unset($_SESSION['errors']);
+						}
+						?>
+					</div>
+					<!-- end login errors -->
+				</div>
+				<div class="row">
+					<div class="col-xs-2">
+					</div>
+					<div class="col-xs-8">
+						<!-- login -->
+						<h1>Login</h1>
+						<form role="form" action="mywall_process.php" method="post">
+							<div class="form-group">
+								<label for="email">Email: </label>
+								<input name="email" type="email" class="form-control">
+							</div>
+							<div class="form-group">
+								<label for="password">Password: </label>
+								<input name="password" type="password" class="form-control">
+							</div>
+							<input type="hidden" name="action" value="login">
+							<button type="submit" class="btn btn-primary">Login</button>
+						</form>
+					</div>
+					<div class="col-xs-2">
+					</div>
+				</div>
+			</div>
 		</div>
-		<!-- login -->
-		<h1>Login</h1>
-		<form action="mywall_process.php" method="post">
-			<label for="email">Email: </label>
-			<input name="email" type="email"><br>
-
-			<label for="password">Password: </label>
-			<input name="password" type="password"><br>
-
-			<input type="hidden" name="action" value="login">
-			<input type="submit" value="Login">
-		</form>
 	</body>
 </html>
