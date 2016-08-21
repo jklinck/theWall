9bd1e162d96da4d0f17bf88188fcadf67e40efa3
jklinck <joeklinck@hotmail.com>
2016-08-21 16:42:49 -0700

    removed inline styles from mywall.php

--- mywall.php
+++ mywall.php
@@ -9,11 +9,6 @@ session_start();
 		<link rel="stylesheet" type="text/css" href="mywall.css">
 		<!-- Latest compiled and minified CSS -->
 		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
-		<style>
-			.red{
-				color:red;
-			}
-		</style>
 	</head>
 	<body>
 		<div class="container">
